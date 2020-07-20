0f829278598a9d4d7e0db45ada66bd7d28bb6185
Silvarney Henrique <silvarney@gmail.com>
2020-07-20 20:35:16 -0300

    #22 - correcoes pedido produto

--- app/Http/Controllers/api/empresa/GetPedidoProdutoController.php
+++ app/Http/Controllers/api/empresa/GetPedidoProdutoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api\empresa;
 
 use App\Http\Controllers\Controller;
+use App\Models\Pedido;
 use App\Models\PedidoProduto;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -35,8 +36,19 @@ class GetPedidoProdutoController extends Controller
 
     public function update(Request $request, $id)
     {
+        //atualizar produto
+        $dados_produto['status'] = $request->status;
+        
         $pedido_produto = PedidoProduto::findOrFail($id);
-        $pedido_produto->update($request->all());
+        $pedido_produto->update($dados_produto);
+
+        //atualizar pedido
+        $pedido_valor = Pedido::findOrFail($request->pedido_id);
+        
+        $dados_pedido['valor_total'] = $request->valor_total;
+        $dados_pedido['quantidade'] = $request->quantidade;
+
+        $pedido_valor->update($dados_pedido);
     }
 
     public function destroy($id)
